b9a63dc7844b19c5188a8d10d9b1555780cd7126
alex <sasha_xarlamov@mail.ru>
2022-09-26 21:45:53 +0300

    commit

--- main.cc
+++ main.cc
@@ -16,8 +16,6 @@ std::vector<std::string> ReadData() {
     strings.push_back(line);
   }
 
-  file.close();
-
   return strings;
 }
 
@@ -36,22 +34,40 @@ std::vector<Packet> MakePackets(std::vector<std::string>& strings)  {
   return packets;
 }
 
-void WritePackets(std::vector<Packet>& packets) {
-  std::string file_name{"packets.txt"};
+void WritePackets(std::vector<Packet>& packets, const std::string& file_name) {
   std::ofstream file{file_name};
 
   for (auto &packet : packets) {
     file << packet;
   }
+}
 
-  file.close();
+std::vector<Packet> ReadPackets() {
+  std::vector<Packet> packets;
+
+  std::string file_name{"packets.txt"};
+  std::ifstream file{file_name};
+
+  while (!file.eof()) {
+    std::string line;
+    getline(file, line);
+    if (line.empty())
+      break;
+    Packet packet;
+    packet.LoadFromPacket(line);
+    packets.emplace_back(packet);
+  }
+
+  return packets;
 }
 
 int main() {
   std::vector<std::string> strings{ReadData()};
   std::vector<Packet> packets{MakePackets(strings)};
-  WritePackets(packets);
+  WritePackets(packets, "packets.txt");
 
+  packets = ReadPackets();
+  WritePackets(packets, "packets_res.txt");
 
   return 0;
 }
--- packet.h
+++ packet.h
@@ -25,7 +25,7 @@ struct Packet {
   Byte end{0x00};
 };
 
-static Byte ComputeHash(const std::string& str) {
+static Byte CalculateHash(const std::string& str) {
   return CRC::Calculate(str.c_str(), str.length(), CRC::CRC_8());
 }
 
@@ -37,28 +37,30 @@ inline void Packet::LoadFromData(std::string& str, int index) {
   ss >> payload2.data;
   payload2.number = index + 1;
 
-  crc = ComputeHash(payload1 + payload2);
+  crc = CalculateHash(payload1 + payload2);
 }
 
 inline void Packet::LoadFromPacket(std::string& str) {
   std::stringstream ss{str};
 
+  ss >> start;
   ss >> payload1.data;
   ss >> payload1.number;
   ss >> payload2.data;
   ss >> payload2.number;
   ss >> crc;
+  ss >> end;
 }
 
 inline std::ostream& operator<<(std::ostream& os, const Packet& packet) {
   os << packet.start;
-  os << ' ';
+//  os << ' ';
   os << packet.payload1;
-  os << ' ';
+//  os << ' ';
   os << packet.payload2;
-  os << ' ';
+//  os << ' ';
   os << packet.crc;
-  os << ' ';
+//  os << ' ';
   os << packet.end;
 
   os << std::endl;
